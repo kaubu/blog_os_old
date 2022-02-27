f3c81d6bb5373992c7c0b95c7e0a4ab23d947e09
anon <anon@gmail.com>
2022-02-27 21:04:52 +1300

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+// Turn off the standard library
+#![no_std]
+// Don't use the normal entry point chain
+#![no_main]
+
+// Imports
+use core::panic::PanicInfo;
+
+// This function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
