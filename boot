6603140a83044724f4b3efdabdf71a8a24ec525b
anon <anon@gmail.com>
2022-02-27 22:27:14 +1300

    Update

---
+++ boot
@@ -0,0 +1,2 @@
+#!/bin/bash
+qemu-system-x86_64 -drive format=raw,file=target/x86_64-blog_os/debug/bootimage-blog_os.bin
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] // Don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    // Raw pointer to the buffer location
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
