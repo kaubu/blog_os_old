b309443c18cb71665b140dc1a3c9202c5c6e61b4
Anonymous <>
2022-02-28 19:53:18 +1300

    v0.3.0

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # blog os
 This is an OS build following the instructions at [https://os.phil-opp.com/](https://os.phil-opp.com/)
 ## Prerequisites
+Install these programs
+* Rust and cargo
+* Qemu (and have it in path too)
+
+Install the `bootimage` tool
+```
+cargo install bootimage
+```
 Install the `llvm-tools-preview` component
 ```
 rustup component add llvm-tools-preview
+```
+Run it
+```
+cargo run
+```
+or build the .bin file
+```
+cargo build
 ```
--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,9 +1,12 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::PageFaultErrorCode;
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use spin;
+
 use crate::{println, print};
 use crate::gdt;
+use crate::hlt_loop;
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
@@ -24,6 +27,9 @@ lazy_static! {
             .set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()]
             .set_handler_fn(keyboard_interrupt_handler);
+
+        
+        idt.page_fault.set_handler_fn(page_fault_handler);
 
         idt
     };
@@ -86,6 +92,19 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed address: {:?}", Cr2::read());
+    println!("Error code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 #[test_case]
